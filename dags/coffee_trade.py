77e78a55a49e240e507ac82982a63f1c34a84c68
Victor Goncalves <60231827+victor1cg@users.noreply.github.com>
2024-12-07 17:58:00 -0300

    Update coffee_trade.py

--- dags/coffee_trade.py
+++ dags/coffee_trade.py
@@ -18,15 +18,15 @@ default_args = {
     'retries': 1
 }
 
-def download_s3_file(bucket_name:str, key:str):
-    s3_hook = S3Hook(aws_conn_id = 'aws_default')
-    file_obj = s3_hook.get_key(key=key , bucket_name = bucket_name)
+def download_s3_file(bucket_name: str, key: str):
+    s3_hook = S3Hook(aws_conn_id='aws_default')
+    file_obj = s3_hook.get_key(key=key, bucket_name=bucket_name)
     content = file_obj.get()['Body'].read().decode('utf-8')
     return content
 
 def process_data(**kwargs):
-    ti=kwargs['ti']
-    csv_content = ti.xcom_pull(task_id = 'download_file')
+    ti = kwargs['ti']
+    csv_content = ti.xcom_pull(task_id='download_file')
 
     df = pd.read_csv(StringIO(csv_content))
 
@@ -34,7 +34,7 @@ def process_data(**kwargs):
     # Return the modified DataFrame as a CSV string
     return df.to_csv(index=False)
 
-    # INSERTING DATA ##########
+# INSERTING DATA ##########
 def insert_into_postgres(table: str, **kwargs):
     ti = kwargs['ti']
     processed_csv = ti.xcom_pull(task_ids='process_data')
@@ -47,52 +47,7 @@ def insert_into_postgres(table: str, **kwargs):
 
     # Insert rows into the table
     for _, row in df.iterrows():
-            cursor.execute(
-                f"""
-                INSERT INTO {table} (transaction_id, transaction_date, transaction_time, store_id, store_location, 
-                product_id, transaction_qty, unit_price, product_category, product_type, product_detail, size, total_bill, 
-                month_name, day_name, hour, day_of_week, month, time_stamp)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                """,
-                tuple(row)
-            )
-    conn.commit()
-    cursor.close()
-
-with DAG(
-    'tutorial',
-    default_args=default_args,
-    description='Load data from S3 to Postgres with TimeStamp Column',
-    schedule_interval=None,
-    start_date=datetime(2024,11,1),
-    catchup = False,
-    tags=['example'],
-) as dag:
-     
-    download_file = PythonOperator(
-          task_id = 'download_file',
-          python_callable = 'download_s3_file',
-          op_kwargs = {
-               'bucket_name':'airflow-files-goncavic01',
-               'key':' coffee_data/'
-          })
-
-    process_data = PythonOperator(
-        task_id='process_data',
-        python_callable=process_data,
-        )
-
-    insert_data = PythonOperator(
-        task_id='insert_data',
-        python_callable=insert_into_postgres,
-        op_kwargs={
-            'table': 'nasa.rovers.coffee_trade'
-        },
-     )
-    
-    start = EmptyOperator(task_id = 'start') 
-    
-    end = EmptyOperator (task_id = 'end')
-
-    #set task dependecies
-    start >> download_file >> process_data >> insert_data >> end
+        cursor.execute(
+            f"""
+            INSERT INTO {table} (transaction_id, transaction_date, transaction_time, store_id, store_location, 
+            product_id, transaction_qty, unit_price, product_category, product_type, product_detail, size, to
